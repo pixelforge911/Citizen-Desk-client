7b589533197463a239ab6a323f8b0a9a5662d0a0
pixelforge911 <sinipinnikalyan@gmail.com>
2026-01-13 18:25:34 +0530

    final touch

--- src/components/Home/DetailPopup.jsx
+++ src/components/Home/DetailPopup.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../../styles/DetailPopup.css";
+import "../../styles/Detailpopup.css";
 import axios from "axios";
 
 function DetailPopup({ item, onClose }) {
